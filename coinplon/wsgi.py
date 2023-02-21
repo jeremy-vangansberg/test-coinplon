b3b24cd80d91f583c6277766b1160ef9e2c79fa2
Jeremy <jeremy.vangansberg@gmail.com>
2023-02-21 18:18:24 +0100

    first commit

---
+++ coinplon/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for coinplon project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from coinplon.settings import base
+
+
+if base.DEBUG :
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coinplon.settings.dev')
+else : 
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coinplon.settings.prod')
+
+
+application = get_wsgi_application()
